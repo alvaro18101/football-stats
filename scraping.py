c1f9b6adad1aaadf00c1cae1e35de46547ddf23f
Alvaro Siesquen <alvaro.siesquen@unmsm.edu.pe>
2025-08-18 20:21:53 -0500

    feat: Scrape match info and team stats

--- scraping.py
+++ scraping.py
@@ -24,5 +24,35 @@ driver.quit()
 
 soup  = BeautifulSoup(html, 'html.parser')
 
+championship = soup.find('span', {'class': 'imso-loa imso-ln'}).text
+# championship = soup.find('span', {'class': 'imso-hide-overflow'})
+# date = 
+
 result1 = soup.find('div', {'class': 'imso_mh__l-tm-sc imso_mh__scr-it imso-light-font'}).text
 result2 = soup.find('div', {'class': 'imso_mh__r-tm-sc imso_mh__scr-it imso-light-font'}).text
+
+team_stats = soup.find_all('tr', {'class': 'MzWkAb'})
+stats_1 = []
+stats_2 = []
+
+for i in team_stats:
+    team_stats_contents = i.contents
+    stats_1.append(team_stats_contents[0].text)
+    stats_2.append(team_stats_contents[2].text)
+
+
+goals_info_1 = ''
+goals_info_2 = ''
+if int(result1)>0:
+    goals_info_1 = soup.find('div', {'class': 'imso_gs__tgs imso_gs__left-team'})
+if int(result2)>0:
+    goals_info_2 = soup.find('div', {'class': 'imso_gs__tgs imso_gs__right-team'})
+
+# print()
+# print(type(goals_info_1))
+print((goals_info_2.text))
+
+if int(stats_1[6]) + int(stats_1[7]) + int(stats_2[6]) + int(stats_2[7]) > 0:
+    cards_info = soup.find('div', {'class': 'imso_gs__cs-cont imso-medium-font'})
+
+print(cards_info.text)
