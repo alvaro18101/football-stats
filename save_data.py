eeee029e3329f1a816e8c08de922f2b6b3f11a9d
Alvaro Siesquen <alvaro.siesquen@unmsm.edu.pe>
2026-02-14 12:41:47 -0500

    Add flags to save_data.py

--- save_data.py
+++ save_data.py
@@ -4,32 +4,32 @@ import pandas as pd
 def save_data(url):
     columns = ['match_number', 'date', 'competition', 'rival_name', 'local', 'gf', 'ga', 'info_goals', 'shots', 'shots_on_goal', 'possession', 'passing', 'passing_accuracy', 'fouls', 'yellow_cards', 'red_cards', 'offside', 'corner']
 
-    team_1_name, data_team_1, team_2_name, data_team_2 = scrape_match(url)
-
+    team_name_1, data_team_1, team_name_2, data_team_2 = scrape_match(url)
     try:
-        df1 = pd.read_excel(f'raw_data/raw_{team_1_name}.xlsx')
+        df1 = pd.read_excel(f'raw_data/raw_{team_name_1}.xlsx')
         data_team_1[0] = list(df1['match_number'])[-1] + 1
     except:
         df1 = pd.DataFrame(columns=columns)
     
     try:
-        df2 = pd.read_excel(f'raw_data/raw_{team_2_name}.xlsx')
+        df2 = pd.read_excel(f'raw_data/raw_{team_name_2}.xlsx')
         data_team_2[0] = list(df2['match_number'])[-1] + 1
     except:
         df2 = pd.DataFrame(columns=columns)
-
     if data_team_1[1] not in list(df1['date']):
         df1.loc[-1] = data_team_1
         df2.loc[-1] = data_team_2
 
-    df1.to_excel(f'raw_data/raw_{team_1_name}.xlsx', index=False)
-    df2.to_excel(f'raw_data/raw_{team_2_name}.xlsx', index=False)
-    print('---Saved data---')
+    df1.to_excel(f'raw_data/raw_{team_name_1}.xlsx', index=False)
+    df2.to_excel(f'raw_data/raw_{team_name_2}.xlsx', index=False)
+    print('---Saved data---\n')
 
-with open('links/barcelona.txt', 'r') as text_file:
+current_team_name = 'barcelona'
+
+with open(f'links/{current_team_name}.txt', 'r') as text_file:
     j = 1
     for i in text_file.readlines():
         if i != '\n':
-            print(j)
+            print(j, end='. ')
             j+= 1
             save_data(i)
